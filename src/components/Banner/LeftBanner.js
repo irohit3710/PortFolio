fafa4d781d0b556d59bdd2b942128840e7c749f5
Rohit Panwar <133122171+irohit3710@users.noreply.github.com>
2023-11-27 00:41:23 +0530

    v1.0.4

--- src/components/Banner/LeftBanner.js
+++ src/components/Banner/LeftBanner.js
@@ -26,9 +26,7 @@ const LeftBanner = () => {
           />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wide">
-          I use animation as a third dimension by which to simplify experiences
-          and kuiding thro each and every interaction. I'm not adding motion
-          just to spruce things up, but doing it in ways that.
+          MERN stack developers are proficient in utilizing MongoDB, Express.js, React.js, and Node.js to build robust and scalable full-stack web applications. With expertise across both frontend and backend technologies, MERN developers excel in creating seamless, modern, and feature-rich digital experiences.
         </p>
       </div>
      {/* Media */}
